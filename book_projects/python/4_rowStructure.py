209708946a059b6f666b4a895d808ffbd1273675
Carlos Enrique Jaramillo <carlos.jaramillo@optima.net.co>
2024-08-23 15:41:33 -0500

    (root, DEV,PY) renombre de archivos  Changes to be committed: 	renamed:    book_projects/python/mnmcount.ipynb -> book_projects/python/1_mnmcount.ipynb 	renamed:    book_projects/python/mnmcount.py -> book_projects/python/1_mnmcount.py 	renamed:    book_projects/python/mnmcount.read -> book_projects/python/1_mnmcount.read 	renamed:    book_projects/python/Structure_apis.ipynb -> book_projects/python/2_Structure_apis.ipynb 	renamed:    book_projects/python/Structure_apis.py -> book_projects/python/2_Structure_apis.py 	renamed:    book_projects/python/schemaStructures.py -> book_projects/python/3_schemaStructures.py 	new file:   book_projects/python/4_rowStructure.py

---
+++ book_projects/python/4_rowStructure.py
@@ -0,0 +1,18 @@
+from pyspark.sql import Row
+from pyspark.sql import SparkSession
+
+
+spark = (SparkSession
+            .builder
+            .appName("RowDataFrame-3_6")
+            .getOrCreate()
+)
+
+
+blog_row = Row(6, "Reynold", "Xin", "https://tinyurl.6", 255568, "3/2/2015",["twitter", "LinkedIn"])
+# access using index for individual items
+print(blog_row[1])
+rows = [Row("Matei Zaharia", "CA"), Row("Reynold Xin", "CA")]
+
+authors_df = spark.createDataFrame(rows, ["Authors", "State"])
+authors_df.show()
